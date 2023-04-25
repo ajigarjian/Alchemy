5a98387c059d99e7ee33ec96c32af75f7575540b
Ari Jigarjian <ajigarjian@gmail.com>
2023-04-25 18:00:33 -0400

    added login page, view, url. Need to change user model(s)

--- AlchemyProject/AlchemyApp/urls.py
+++ AlchemyProject/AlchemyApp/urls.py
@@ -4,6 +4,7 @@ from . import views
 app_name = "alchemy"
 urlpatterns = [
      path("", views.index, name="index"),
+     path("access", views.login, name="access"),
      path("overview", views.overview, name="overview"),
      path("questions/<str:control_family_name>", views.questions, name="questions"),
 
