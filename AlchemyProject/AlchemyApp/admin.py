0a0e74731c24deeaa0cf18b248bf41de1ae48a6d
Ari Jigarjian <ajigarjian@gmail.com>
2023-05-01 21:06:25 -0400

    adding form functionality for org overview

--- AlchemyProject/AlchemyApp/admin.py
+++ AlchemyProject/AlchemyApp/admin.py
@@ -1,14 +1,14 @@
 from django.contrib import admin
-from .models import CustomUser, Client, NISTControl, Question, Answer, ControlFamily, InformationCategory, InformationSubCategory
+from .models import CustomUser, Client, System, NISTControl, Question, Answer, ControlFamily, InformationCategory, InformationSubCategory
 
 # Register your models here.
 
 admin.site.register(CustomUser)
 admin.site.register(Client)
+admin.site.register(System)
 admin.site.register(NISTControl)
 admin.site.register(ControlFamily)
 admin.site.register(Question)
 admin.site.register(Answer)
 admin.site.register(InformationCategory)
 admin.site.register(InformationSubCategory)
-
