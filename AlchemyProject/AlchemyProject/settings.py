4f5f3445c8dd41453b9dd7ff184e59b715244b1f
Ari Jigarjian <ajigarjian@gmail.com>
2023-06-12 09:20:10 -0400

    fixed static files configuration

--- AlchemyProject/AlchemyProject/settings.py
+++ AlchemyProject/AlchemyProject/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().strict().parent.parent
 
 
 # Quick-start development settings - unsuitable for production
@@ -143,6 +143,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
+
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
