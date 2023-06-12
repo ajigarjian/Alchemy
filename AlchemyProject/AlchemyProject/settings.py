26559e6afec14c51671db0b32f4224c453f01f8e
Ari Jigarjian <ajigarjian@gmail.com>
2023-06-12 09:35:08 -0400

    more changes

--- AlchemyProject/AlchemyProject/settings.py
+++ AlchemyProject/AlchemyProject/settings.py
@@ -183,11 +183,10 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-
 # The `DYNO` env var is set on Heroku CI, but it's not a real Heroku app, so we have to
 # also explicitly exclude CI:
 # https://devcenter.heroku.com/articles/heroku-ci#immutable-environment-variables
-IS_HEROKU_APP = "DYNO" in os.environ and not "CI" in os.environ
+IS_HEROKU_APP = os.environ.get('DYNO') is not None and os.environ.get('CI') is None
 
 # On Heroku, it's safe to use a wildcard for `ALLOWED_HOSTS``, since the Heroku router performs
 # validation of the Host header in the incoming HTTP request. On other platforms you may need
