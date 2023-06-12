1391b33fb69e86c7ad8fc737ba59a2d510deada7
Ari Jigarjian <ajigarjian@gmail.com>
2023-06-12 19:27:33 -0400

    test 2

--- AlchemyProject/AlchemyProject/settings.py
+++ AlchemyProject/AlchemyProject/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 import os
 import secrets
-import dj_database_ur
+import dj_database_url
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
