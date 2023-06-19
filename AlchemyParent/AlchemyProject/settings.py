d671af6ad707bdf216b6c8c141c2af3a03c0b2d5
Ari Jigarjian <ajigarjian@gmail.com>
2023-06-19 15:13:30 -0400

    migrated to postgresql from sqlite

--- AlchemyParent/AlchemyProject/settings.py
+++ AlchemyParent/AlchemyProject/settings.py
@@ -108,9 +108,13 @@ WSGI_APPLICATION = 'AlchemyProject.wsgi.app'
 # When running locally in development or in CI, a sqlite database file will be used instead
 # to simplify initial setup. Longer term it's recommended to use Postgres locally too.
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'test',
+        'USER': 'postgres',
+        'PASSWORD': 'test123',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
@@ -125,7 +129,7 @@ AUTH_USER_MODEL = 'AlchemyApp.CustomUser'
 # Changing the default login url from /accounts/login to /login for @login_required.
 LOGIN_URL = '/login'
 
-# Password validation
+# Password validation   
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
